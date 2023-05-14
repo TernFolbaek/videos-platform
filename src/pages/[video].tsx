45a0073913b4ca7ea8920cfd41d2ed371adee539
Tern Folbaek <tfolbaek@gmail.com>
2023-05-14 23:50:08 +0200

    fixed id

--- src/pages/[video].tsx
+++ src/pages/[video].tsx
@@ -1,53 +1,47 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { useRouter } from "next/router";
 import { videoStore } from "./useStore";
 
 const VideoId = () => {
-  const [video, setVideo] = useState({title: '', description: '', video_path : '', thumbnail_path: ''});
+  const [video, setVideo] = useState({ title: '', description: '', video_path: '', thumbnail_path: '' });
   const { videoId } = videoStore();
+  const isMounted = useRef(false);
 
   useEffect(() => {
-    (async () => {
-      try {
-        const response = await fetch("/api/specificVideo", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ videoId: videoId }),
-        });
+    if (!isMounted.current) {
+      isMounted.current = true;
+      (async () => {
+        try {
+          const response = await fetch("/api/specificVideo", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ videoId: videoId }),
+          });
 
-        const data = await response.json();
-        const removedPath = data.video_path.substring(data.video_path.indexOf("/uploads"));
-        data.video_path = removedPath;
-        setVideo(data);
-        console.log(data);
-      } catch (error) {
-        console.log(error);
-      }
-    })();
-
+          const data = await response.json();
+          console.log(typeof(data.video_path))
+          data.video_path = `/uploads/${data.video_path.split('/').slice(-1)[0]}`;
+          data.thumbnail_path = `/uploads/${data.thumbnail_path.split('/').slice(-1)[0]}`
+          setVideo(data);
+        } catch (error) {
+          console.log(error);
+        }
+      })();
+    }
   }, [videoId]);
 
-  if(video){
-    return(
-      <div>
-        <h2>{video.title}</h2>
-        <h4>{video.description}</h4>
-        <h3>{video.video_path}</h3>
-        <video width="320" height="240" controls>
+  return (
+    <div >
+      <h1>{video.title}</h1>
+      <h4>{video.description}</h4>
+      <video width="320" height="240" controls>
             <source src={video.video_path} type="video/mp4" />
             Your browser does not support the video tag.
-        </video>
-      </div>
-    )
-  }
-  else{
-  return (
-    <div className="video-container">
-      <h1>hello</h1>
+      </video>
     </div>
   );
-}}
+};
 
 export default VideoId;
