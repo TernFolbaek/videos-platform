ef268de776e861ea2aaeff70d01d6bb7af269679
Tern Folbaek <tfolbaek@gmail.com>
2023-05-16 15:35:02 +0200

    user information reload

--- src/pages/user-information.tsx
+++ src/pages/user-information.tsx
@@ -1,44 +1,79 @@
 import { useStore } from "./useStore";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
-const  UserInformation = () => {
-    const {user, userId} = useStore()
-    const setUser = useStore((state) => state.setUser)
-    const setUserId = useStore((state) => state.setUserId)
-    console.log(userId)
-    const logOut = async () => {
-        console.log('here')
-        try {
-            const response = await fetch('/api/logout', {
-                method: 'DELETE',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': `Bearer ${localStorage.getItem('jwt')}` // Replace with how you're storing your JWT
-                },
-            });
-            if (response.ok) {
-                // Reset user and userId in the store
-                console.log('nice')
-                setUser('');
-                setUserId(null);
-                window.location.href = '/'
-            } else {
-                console.error('Logout failed');
-            }
-        } catch (error) {
-            console.error('An error occurred:', error);
-        }
+const UserInformation = () => {
+  const { user, userId } = useStore();
+  const setUser = useStore((state) => state.setUser);
+  const setUserId = useStore((state) => state.setUserId);
+
+  const [storedUser, setStoredUser] = useState('');
+  const [storedUserId, setStoredUserId] = useState('');
+
+  useEffect(() => {
+    const localUser = localStorage.getItem('user') || '';
+    const localUserId = localStorage.getItem('userId') || '';
+
+    if (localUser) {
+      setUser(localUser);
+      setStoredUser(localUser);
     }
-    
 
-    
-    return(
-       <div className="userInfo">
-        <h1>User Info</h1>
-        <p>username: {user}</p>
-        <p>user ID:  {userId}</p>
-        <button onClick={logOut}>Log Out</button>
-       </div>
-    )
+    if (localUserId) {
+      setUserId(localUserId);
+      setStoredUserId(localUserId);
+    }
+
+    if (!localUserId && localUser) {
+      (async () => {
+        try {
+          const response = await axios.post('/api/userId', { username: localUser });
+          const userIdFromResponse = response.data.userId;
+          if (userIdFromResponse) {
+            setUserId(userIdFromResponse);
+            setStoredUserId(userIdFromResponse);
+            localStorage.setItem('userId', userIdFromResponse);
+          }
+        } catch (err) {
+          console.error(err);
+        }
+      })();
+    }
+  }, []);
+
+  const logOut = async () => {
+    try {
+      const response = await fetch('/api/logout', {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${localStorage.getItem('jwt')}`
+        },
+      });
+      if (response.ok) {
+        setUser('');
+        setUserId('');
+        setStoredUser('');
+        setStoredUserId('');
+        localStorage.removeItem('user');
+        localStorage.removeItem('userId');
+        window.location.href = '/'
+      } else {
+        console.error('Logout failed');
+      }
+    } catch (error) {
+      console.error('An error occurred:', error);
+    }
+  }
+
+  return (
+    <div className="userInfo">
+      <h1>User Info</h1>
+      <p>username: {storedUser}</p>
+      <p>user ID: {storedUserId}</p>
+      <button onClick={logOut}>Log Out</button>
+    </div>
+  )
 }
 
 export default UserInformation;
