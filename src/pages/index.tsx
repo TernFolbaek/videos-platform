ae47b53ad80ece8d866a5ca2185ef690a55a8313
Tern Folbaek <tfolbaek@gmail.com>
2023-05-21 14:44:46 +0200

    url change

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -41,7 +41,7 @@ const VideoList = () => {
   useEffect(() => {
     const fetchVideos = async () => {
       try {
-        const response = await axios.get('http://localhost:2000/api/videos');
+        const response = await axios.get('https://1bwbo035k6.execute-api.eu-north-1.amazonaws.com/staging/api/videos');
         console.log(response)
         setVideos(
           response.data.map((video: any) => ({
